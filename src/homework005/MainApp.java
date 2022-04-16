e7d6768b71471a02c0e1de126b3b1a7e0b9a13b7
BİLAL <bllsfk.3984@gmail.com>
2022-04-16 03:09:19 +0300

    Homework005 has been completed by Bilal Şafak.

---
+++ src/homework005/MainApp.java
@@ -0,0 +1,18 @@
+package homework005;
+
+public class MainApp {
+    public static void main(String[] args) {
+
+        Wall wall = new Wall(5,4);
+        System.out.println("area= " + wall.getArea());
+
+        wall.setHeigth(-1.5);
+        System.out.println("width= "+wall.getWidth());
+        System.out.println("heigth= "+wall.getHeigth());
+        System.out.println("area= "+wall.getArea());
+    }
+}
+/* OUTPUT:  area= 20.0
+            width= 5.0
+            heigth= 0.0
+             area= 0.0    */
---
+++ src/homework005/Wall.java
@@ -0,0 +1,39 @@
+package homework005;
+
+public class Wall {
+    private double width;
+    private double heigth;
+
+    public Wall() {
+    }
+
+    public Wall(double width, double heigth) {
+        this.width = width;
+        this.heigth = heigth;
+    }
+
+    public double getWidth() {
+        return width;
+    }
+
+    public double getHeigth() {
+        return heigth;
+    }
+
+    public void setWidth(double width) {
+        if (width < 0) {
+            width = 0.0;
+            this.width = width;
+        }
+    }
+
+    public void setHeigth(double heigth) {
+        if (heigth < 0) {
+            heigth = 0.0;
+            this.heigth = heigth;
+        }
+    }
+    public double getArea(){
+        return width*heigth;
+    }
+}
