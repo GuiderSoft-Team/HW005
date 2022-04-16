9571d17ee1d5d1483127322ccf3541f1f23ba9a9
archer1903 <98432117+archer1903@users.noreply.github.com>
2022-04-16 11:33:37 +0300

    Huseyin Kartal

--- src/MainApp.java
+++ src/MainApp.java
@@ -1,5 +1,6 @@
 public class MainApp {
     public static void main(String[] args) {
+
          Wall wall = new Wall(5,4);
          System.out.println("area= " + wall.getArea());
 
--- src/Wall.java
+++ src/Wall.java
@@ -1,4 +1,5 @@
 public class Wall {
+
     double width;
     double height;
 
