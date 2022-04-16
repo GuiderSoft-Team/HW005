7e48e97903c79409d5cfe1dfafe88ece61517d25
orhan374 <orhndgnr@gmail.com>
2022-04-16 18:26:40 +0300

    orhan did homework5.

--- src/MainApp.java
+++ src/MainApp.java
@@ -1,5 +1,14 @@
 public class MainApp {
     public static void main(String[] args) {
-        //TODO: Test your code
+        //: Test your code
+        Wall wall = new Wall(5,4);
+        System.out.println(wall.getWidth());
+        System.out.println(wall.getHeight());
+        System.out.println("area= " + wall.area());
+
+         wall.setHeight(-1.5);
+        System.out.println("width= " + wall.getWidth());
+        System.out.println("height= " + wall.getHeight());
+        System.out.println("area= " + wall.area());
     }
 }
