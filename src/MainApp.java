ee0f4cdf7dbde6073a176c482f9e3da02e78230c
hasanozye <hasanozyer06@gmail.com>
2022-04-17 14:13:29 +0300

    HW005 has been created.

--- src/MainApp.java
+++ src/MainApp.java
@@ -4,7 +4,7 @@ public class MainApp {
         Wall wall = new Wall(5,4);
          System.out.println("area= " + wall.getArea());
 
-         wall.setHeight(-1.5);
+         wall.setHeight(-3.5);
          System.out.println("width= " + wall.getWidth());
          System.out.println("height= " + wall.getHeight());
          System.out.println("area= " + wall.getArea());
