0c589b738f7e8987630e89d6a3acaf95c2ff9971
osoka46 <osman.bozdag@yahoo.com>
2022-04-18 17:58:06 +0200

    saygilar

--- src/MainApp.java
+++ src/MainApp.java
@@ -1,5 +1,18 @@
+import java.util.Scanner;
+
 public class MainApp {
     public static void main(String[] args) {
-        //TODO: Test your code
+        Scanner input=new Scanner(System.in);
+        System.out.println("type width ");
+        double width=input.nextDouble();
+        System.out.println("type heigt");
+        double height= input.nextDouble();
+
+        Wall nesne1=new Wall(width,height);
+        System.out.println("width:"+nesne1.getWidth());
+        System.out.println("height:"+nesne1.getHeight());
+        System.out.println("area:" +nesne1.getArea());
+
+
     }
 }
